01b2078c25301fe33315fb3c80790056e8b702fd
Xinyi-UW <148395443+Xinyi-UW@users.noreply.github.com>
2023-10-31 18:17:08 -0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -2,7 +2,6 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
 def make_empty_board():
     return [
         [None, None, None],
@@ -10,13 +9,29 @@ def make_empty_board():
         [None, None, None],
     ]
 
-
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    for row in board:
+        if all(cell == 'X' for cell in row):
+            return 'X'
+        elif all(cell == 'O' for cell in row):
+            return 'O'
 
+    for col in range(3):
+        if all(board[row][col] == 'X' for row in range(3)):
+            return 'X'
+        elif all(board[row][col] == 'O' for row in range(3)):
+            return 'O'
+
+    if all(board[i][i] == 'X' for i in range(3)) or all(board[i][2 - i] == 'X' for i in range(3)):
+        return 'X'
+
+    if all(board[i][i] == 'O' for i in range(3)) or all(board[i][2 - i] == 'O' for i in range(3)):
+        return 'O'
+
+    return None
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return 'O' if player == 'X' else 'X'
