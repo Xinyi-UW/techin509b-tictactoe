f9ce5ee60079e765053b2059d299ebabf43e2e73
Xinyi-UW <148395443+Xinyi-UW@users.noreply.github.com>
2023-10-31 18:19:24 -0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,18 +2,34 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, get_winner, other_player
 
-
-# Reminder to check all the tests
+def display_board(board):
+    for row in board:
+        print(" ".join(cell if cell is not None else '-' for cell in row))
+    print()
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+    player = 'X'
+
+    while winner is None:
+        display_board(board)
+        print(f"Player {player}'s turn")
+        row = int(input("Enter row (0, 1, or 2): ")
+        col = int(input("Enter column (0, 1, or 2): ")
+
+        if board[row][col] is not None:
+            print("Cell already occupied. Try again.")
+            continue
+
+        board[row][col] = player
+        winner = get_winner(board)
+        player = other_player(player)
+
+    display_board(board)
+    if winner:
+        print(f"Player {winner} wins!")
+    else:
+        print("It's a tie!")
